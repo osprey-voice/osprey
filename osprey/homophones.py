23ffdbf499394f42ce4cdbfff028b0ccc5552af4
Caleb Bassi <calebjbassi@gmail.com>
2019-11-27 17:56:54 -0800

    Refactor homophones

--- osprey/homophones.py
+++ osprey/homophones.py
@@ -2,12 +2,12 @@
 
 from pathlib import Path
 
-homophones_filename = "homophones.csv"
-homophones_filepath = Path(__file__).parent / homophones_filename
-
 HOMOPHONES = {}
 
-for line in homophones_filepath.read_text().splitlines():
+HOMOPHONES_FILENAME = "homophones.csv"
+HOMOPHONES_FILEPATH = Path(__file__).parent / HOMOPHONES_FILENAME
+
+for line in HOMOPHONES_FILEPATH.read_text().splitlines():
     words = sorted(line.split(","))
     for word in words:
         HOMOPHONES[word.lower()] = words
