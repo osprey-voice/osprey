77e32a2a2803dfb8187560077158e2c8a40d3822
Caleb Bassi <calebjbassi@gmail.com>
2020-01-31 21:45:13 -0800

    remove digits to words

--- osprey/conversions.py
+++ osprey/conversions.py
@@ -1,6 +1,5 @@
 DIGIT_WORDS = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 WORDS_TO_DIGITS = dict(zip(DIGIT_WORDS, range(10)))
-DIGITS_TO_WORDS = dict(zip(range(10), DIGIT_WORDS))
 
 ORDINALS = {
     "first": "1st",
