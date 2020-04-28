17d1f54810fb6d3175935d5b4395e7ed7c84fc28
Caleb Bassi <calebjbassi@gmail.com>
2020-04-28 13:10:41 -0700

    add pyautogui for cross platform keyboard simulation

--- osprey/pyautogui.py
+++ osprey/pyautogui.py
@@ -1,8 +1,24 @@
 import pyautogui
 
+from . import keys
+
+KEY_MAP = {key: key.lower() for key in keys.KEYS}
+KEY_MAP.update({
+    'Escape': 'esc',
+    'Mute': 'volumemute',
+})
+
 
 def pyautogui_press(key_string):
-    pass
+    keys = key_string.split(' ')
+    if keys[-1].isupper():
+        keys.insert(-1, 'Shift')
+        keys[-1].lower()
+    keys = {key: KEY_MAP[key] for key in keys}
+    for key in keys:
+        pyautogui.keyDown(key)
+    for key in keys[::-1]:  # `[::-1]` reverses list
+        pyautogui.keyUp(key)
 
 
 def pyautogui_insert(custom_string):
